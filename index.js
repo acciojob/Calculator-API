baf7a9c7642e3c5ffc642e0f6c3de4d5e98c20e6
Subhra264 <chakrabortysubhradeep556@gmail.com>
2022-05-29 21:27:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// This file is not to be modified. Please ignore this.
-// We will understand all of this later in the course.
-// DO NOT MODIFY THIS FILE
+// Add the Calculator APIs
 
 const express = require('express');
 const path = require('path');
@@ -8,14 +6,11 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname))
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/main.html'));
 });
+
 //your code here
-app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
-});
 module.exports = app;
